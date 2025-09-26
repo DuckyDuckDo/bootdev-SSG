ae4e922d11a3af9de01453f26038f6346651b93e
David <daviddo2205@gmail.com>
2025-09-26 14:59:41 -0400

    completed text node to html conversion

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
     BOLD = "bold"
@@ -20,3 +21,28 @@ class TextNode():
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+def text_node_to_html_node(text_node): 
+    if not isinstance(text_node, TextNode):
+        raise ValueError("Please Pass in a TextNode Object")
+    if not isinstance(text_node.text_type, TextType):
+        return ValueError("Invalid Text Type")
+    
+    # Maps text type to its corresponding tag
+    tag_map = {
+        "text": None,
+        "bold": "b",
+        "italic": "i",
+        "code": "code",
+        "image": "img",
+        "link": "a"
+    }
+
+    # Handle specific cases for images and links
+    if text_node.text_type.value == "image":
+        return LeafNode(tag = tag_map["image"], value = "", props = {"src": text_node.url, "alt": text_node.text})
+    
+    if text_node.text_type.value == "link":
+        return LeafNode(tag = tag_map["link"], value = text_node.text, props = {"href": text_node.url})
+    
+    return LeafNode(tag = tag_map[text_node.text_type.value], value = text_node.text)
