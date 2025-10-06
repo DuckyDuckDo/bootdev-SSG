28d867362790a203f0ae77cbf24035ae66a2b628
David <daviddo2205@gmail.com>
2025-10-06 15:12:34 -0400

    added copy function

--- src/main.py
+++ src/main.py
@@ -1,7 +1,24 @@
 from textnode import TextNode, TextType
+import os
+import shutil
+
+def copy_files_recursive(source_dir_path, dest_dir_path):
+    if not os.path.exists(dest_dir_path):
+        os.mkdir(dest_dir_path)
+
+    for filename in os.listdir(source_dir_path):
+        from_path = os.path.join(source_dir_path, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        print(f" * {from_path} -> {dest_path}")
+        if os.path.isfile(from_path):
+            shutil.copy(from_path, dest_path)
+        else:
+            copy_files_recursive(from_path, dest_path)
+
+
+
 def main():
-    dummy_textnode = TextNode("This is some anchor text", TextType.LINK, "https://boot.dev")
-    print(dummy_textnode)
+    copy_files_recursive("/Users/daviddo/Desktop/bootdev/bootdev-SSG/static", "/Users/daviddo/Desktop/bootdev/bootdev-SSG/public")
 
 
 if __name__ == "__main__":
